3094eea2d33994b53f6e96a27acb4bde7e15f30e
NHAphisit42 <nhcomsci61@gmail.com>
2024-05-14 11:24:57 +0700

    14/05/2024

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-f@@!u4va)#9%nqwov2uqp(1o@^!gr4+e-^i42x7kdtf%86_gnz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ["http://*.on-acorn.io", "https://*.on-acorn.io"]
+ALLOWED_HOSTS = []
 
 
 
@@ -79,13 +78,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("MARIADB_DATABASE"),
-        "USER": os.getenv("MARIADB_USER"),
-        "PASSWORD": os.getenv("MARIADB_ROOT_PASSWORD"),
-        "HOST": os.getenv("MARIADB_HOST"),
-        "PORT": os.getenv("MARIADB_PORT", 3306),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
